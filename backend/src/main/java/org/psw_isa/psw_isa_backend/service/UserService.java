442531e719f180af7a527f779f7088594e5e87d3
Nemanja Kajzer <Nemanja Kajzer>
2019-11-14 21:49:42 +0100

    Fix userServisa

--- backend/src/main/java/org/psw_isa/psw_isa_backend/service/UserService.java
+++ backend/src/main/java/org/psw_isa/psw_isa_backend/service/UserService.java
@@ -3,7 +3,9 @@ package org.psw_isa.psw_isa_backend.service;
 import org.psw_isa.psw_isa_backend.models.User;
 import org.psw_isa.psw_isa_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
 	@Autowired
