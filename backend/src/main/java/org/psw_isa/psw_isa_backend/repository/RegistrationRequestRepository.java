6b92276f018fc499ecd4303d9ac09442300761de
Nemanja Kajzer <Nemanja Kajzer>
2019-11-22 16:01:15 +0100

    Ispravljen veci deo registration request controllera, ostalo jos samo za accept i decline

--- backend/src/main/java/org/psw_isa/psw_isa_backend/repository/RegistrationRequestRepository.java
+++ backend/src/main/java/org/psw_isa/psw_isa_backend/repository/RegistrationRequestRepository.java
@@ -1,5 +1,7 @@
 package org.psw_isa.psw_isa_backend.repository;
 
+import java.util.List;
+
 import org.psw_isa.psw_isa_backend.models.RegistrationRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,7 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegistrationRequestRepository extends JpaRepository<RegistrationRequest, Long> {
 	
-	Page<RegistrationRequest> findAll(Pageable pageable);
+	List<RegistrationRequest> findAll();
 	
 	public RegistrationRequest findOneById(Long id);
 	
