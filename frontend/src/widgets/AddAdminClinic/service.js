c2c8dc9d808062920ff95bf8c65f33af12285625
MiroslavRA <susajunior@gmail.com>
2019-11-22 02:23:34 +0100

    Dodat drop down

--- frontend/src/widgets/AddAdminClinic/service.js
+++ frontend/src/widgets/AddAdminClinic/service.js
@@ -1,35 +1,24 @@
 import { API_URL } from "./../../config";
 import axios from "axios"
 
-export default class UserService 
+export default class AddAdminClinicService 
 {
     constructor(self) 
     {
         this.self = self;
     }
 
+    static accept(id)
+    {
+        return axios.post(API_URL + "/adminClinic/"+id);
+    }
+
     static list() 
     {
-        return axios.get(API_URL + "/User/");
+        return axios.get(API_URL + "/adminClinic/all/");
     }
-
     static get(id) 
     {
-        return axios.get(API_URL + "/User/" + id);
-    }
-
-    static create(x)
-    {
-        return axios.post(API_URL + "/User/", x);
-    }
-
-    static update(id, data) 
-    {
-        return axios.post(API_URL + "/User/" + id, data);
-    }
-
-    static delete(id) 
-    {
-        return axios.delete(API_URL + "/User/" + id);
+        return axios.get(API_URL + "/adminClinic/" + id);
     }
 }
