3a340e065476d621686bfc34386898befb2ac19b
MiroslavRA <susajunior@gmail.com>
2019-12-05 00:30:43 +0100

    Izmenjene klase Medicine i Diagnosis. Dodata nova klasa Prescription zbog recepta

--- backend/src/main/java/org/psw_isa/psw_isa_backend/models/Prescription.java
+++ backend/src/main/java/org/psw_isa/psw_isa_backend/models/Prescription.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Prescription {
@@ -14,6 +16,8 @@ public class Prescription {
 	
 	private Long id;
 	
+	@ManyToOne
+    @JoinColumn
 	private ArrayList<Medicine> medicines;
 
 	
