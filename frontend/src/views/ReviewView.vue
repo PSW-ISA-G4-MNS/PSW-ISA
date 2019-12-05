c58ab86ba726309cdb26c7525cc373ebc96b8f9b
MiroslavRA <susajunior@gmail.com>
2019-12-05 16:25:32 +0100

    Dodat View za Review

---
+++ frontend/src/views/ReviewView.vue
@@ -0,0 +1,39 @@
+<script>
+
+
+// import widgets for this view here
+import Review from "../widgets/Review/Review.vue"
+
+export default {
+    name: "ReviewView",
+    data: function () {
+            return {
+		data: {}
+	    };
+	},
+    mounted: function () 
+    {
+    },
+    methods: {
+        submit: function (event) 
+        {
+            
+        }
+    },
+    components: {
+    	"Rreview": Review
+    }
+}
+</script>
+
+<template>
+
+<Review/>
+
+</template>
+
+<style scoped> 
+
+
+
+</style>
