c33514a3d4b3d0d24d41d3136a4f6570cdb78b36
MiroslavRA <susajunior@gmail.com>
2020-01-03 23:01:45 +0100

    Dodat widget za kalendar

---
+++ frontend/src/widgets/Calendar/CalendarWidget.vue
@@ -0,0 +1,31 @@
+<script>
+
+
+export default {
+    name: "Calendar",
+    data: function () {
+        return {
+            data: {
+	    },
+
+        };
+    },
+    methods: {
+    	submit: function() 
+	{
+		
+	}
+    }
+}
+</script>
+
+<template>
+    
+    
+
+</template>
+
+<style scoped> 
+
+
+</style>
